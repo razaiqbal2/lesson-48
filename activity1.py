bef64a53255ad7993808c996eaebead741c0a901
razaiqbal2 <duaf0226@gmail.com>
2024-12-24 18:23:08 +0500

    cat and dogs

---
+++ activity1.py
@@ -0,0 +1,19 @@
+class dog:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def make_sound(self):
+        print("Dog barks at strangers")
+
+class cat:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def make_sound(self):
+        print("Cat makes sound meow")
+
+d1=dog("Greg",4)
+c1=cat("Oreo",3)
+
+for animal in (d1,c1):
+    animal.make_sound()
